b4e160aa0fb8c285985c3f46bf70e58611e762ac
vladick <mamrokhavlad@gmail.com>
2022-05-06 16:39:42 +0300

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/customers/', views.customers_list),
+    path('api/customers/<int:pk>', views.customers_detail),
+
+]
